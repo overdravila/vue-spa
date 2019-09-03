254feed62ddf817d0c2bc2437a7c4277d827687e
Andres Avila <andresavilawille@gmail.com>
2019-09-03 15:23:19 -0600

    initial commit

---
+++ src/theme/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+    <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          Follow us on
+          <a
+            href="https://twitter.com/bstavroulakis"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Twitter</a>
+        </div>
+      </div>
+    </footer>
+</template>
